4cb6d7cca8d0e4bd1e949a06a9dbf9c3d1917be9
米川貴也 <zuomujianwu1@gmail.com>
2023-11-11 09:10:19 +0900

    DONE

--- exchage.js
+++ exchage.js
@@ -1,31 +1,33 @@
-const amount = document.getElementById('amount');
-const currency = document.getElementById('currency');
-const convert = document.getElementById('convert');
-const result = document.getElementById('result');
+document.addEventListener("DOMContentLoaded", function () {
+  const amount = document.getElementById("amount");
+  const currency = document.getElementById("currency");
+  const convert = document.getElementById("convert");
+  const result = document.getElementById("result");
 
+  const API_KEY = "pKk17SJODEM6d3oaYNwNyQ==Qb1CozpZ65qf2ded";
+  const apiUrl = `https://api.api-ninjas.com/v1/exchangerate?pair=USD_`;
 
-const API_KEY = ""
-const apiUrl = ""
-
-
-convert.addEventListener('click', () => {
+  convert.addEventListener("click", () => {
     const amountTotal = amount.value;
     const currencyTotal = currency.value;
     const url = apiUrl + currencyTotal;
 
     fetch(url, {
-        headers: {
-            'X-API-KEY': API_KEY
-        }
+      headers: {
+        "X-API-KEY": API_KEY,
+      },
     })
-    .then(res => res.json())
-    .then(data => {
-        const rate =data.rate;
-        const result = amountTotal * rate;
-        result.innerHTML = `${amount} ${currency} = ${result.toFixed(2)} USD`;
-    })
-    .catch(err => {
-        console.error('Result failed', error);
-        result.innerHTML = 'An error accurred please try again later'
-    })
-})
+      .then((res) => res.json())
+      .then((data) => {
+        const rate = data.exchange_rate;
+        const resultPrice = amountTotal * rate;
+        return (result.innerHTML = `${amountTotal} USD = ${resultPrice.toFixed(
+          2
+        )} ${currencyTotal}`);
+      })
+      .catch((err) => {
+        console.error("Result failed", err);
+        result.innerHTML = "An error accurred please try again later";
+      });
+  });
+});
